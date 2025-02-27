2cfb9ceb75325548d0a6f2b39e6626c26a08169a
viki <158042395+vika-myznikova@users.noreply.github.com>
2025-02-27 17:58:03 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-from sokoban import *
 import pygame
 import pygame_menu
 
+from sokoban import *
+
+
 def main():
     pygame.init()
-    surface = pygame.display.set_mode((600, 400))
+    surface = pygame.display.set_mode((1280, 960))
 
     def start_the_game():
         game = Sokoban(1)
@@ -18,17 +20,19 @@ def main():
         game = Sokoban(2)
         game.launch()
 
-    menu = pygame_menu.Menu('Welcome', 400, 300,
-                            theme=pygame_menu.themes.THEME_BLUE)
+    def select_level3():
+        game = Sokoban(3)
+        game.launch()
 
+    menu = pygame_menu.Menu('Sokoban', 1280, 960,
+                            theme=pygame_menu.themes.THEME_GREEN)
 
     menu.add.button('Play', start_the_game)
     menu.add.button('Level-1', select_level1)
     menu.add.button('Level-2', select_level2)
+    menu.add.button('Level-3', select_level3)
     menu.add.button('Quit', pygame_menu.events.EXIT)
     menu.mainloop(surface)
-
-
 
 
 if __name__ == "__main__":
